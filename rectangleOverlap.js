a7d8f636dd7c3b2c96c48d2d31356858ed8cf220
Michael Chiang <michiang@ucsd.edu>
2017-04-04 23:17:01 +0800

    Add rectangleOverlap

---
+++ rectangleOverlap.js
@@ -0,0 +1,40 @@
+/*
+  Write a function to find the rectangular intersection of two given love rectangles. 
+  As with the example above, love rectangles are always "straight" and never "diagonal."
+  More rigorously: each side is parallel with either the x-axis or the y-axis.
+*/
+
+const findRectangleOverlap = (rectangle1, rectangle2) => {
+	let xOverlap = findRangeOverlap( rectangle1.x, rectangle1.width, rectangle2.x, rectangle2.width );
+	let yOverlap = findRangeOverlap( rectangle1.y, rectangle1.height, rectangle2.y, rectangle2.height );
+
+	if ( !xOverlap || !yOverlap ) {
+		console.log( 'No intersection' );
+		return undefined;
+	}
+	else {
+		let overlapRect = {
+			x: xOverlap[0],
+			y: yOverlap[0],
+			width: xOverlap[1],
+			height: yOverlap[1]
+		};
+		return overlapRect;
+	}
+}
+
+
+//Helper function to compute x and y overlap
+
+function findRangeOverlap( point1, range1, point2, range2 )
+{
+	var farStartPoint = Math.max( point1, point2 );
+	var nearEndPoint = Math.min( point1 + range1, point2 + range2 );
+	var overlap = nearEndPoint - farStartPoint;
+	
+	if ( overlap < 0 ) {
+		return [undefined, undefined];
+	}
+	else {
+		return [farStartPoint, overlap];
+	}
